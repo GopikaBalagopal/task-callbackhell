237f9269a3770ffe8aefa13dbf82fc94065ec714
GopikaBalagopal <kvgopikabalagopal@gmail.comm>
2023-03-16 01:16:20 +0530

    task

---
+++ script.js
@@ -0,0 +1,79 @@
+
+setTimeout(function(){
+let div=document.getElementById("countdown")
+div.innerHTML="<b>10</b>"
+
+document.body.appendChild(div)
+
+setTimeout(function(){
+let div=document.getElementById("countdown")
+div.innerHTML="<b>9</b>"
+
+document.body.appendChild(div)
+
+    
+setTimeout(function(){
+let div=document.getElementById("countdown")
+div.innerHTML="<b>8</b>"
+
+document.body.appendChild(div)
+
+setTimeout(function(){
+let div=document.getElementById("countdown")
+div.innerHTML="<b>7</b>"
+
+document.body.appendChild(div)
+
+setTimeout(function(){
+let div=document.getElementById("countdown")
+div.innerHTML="<b>6</b>"
+
+document.body.appendChild(div)
+
+setTimeout(function(){
+console.log("<b>5</b>")
+let div=document.getElementById("countdown")
+ div.innerHTML="<b>5</b>"
+
+document.body.appendChild(div)
+
+setTimeout(function(){
+let div=document.getElementById("countdown")
+div.innerHTML="<b>4</b>"
+
+document.body.appendChild(div)
+
+setTimeout(function(){
+let div=document.getElementById("countdown")
+div.innerHTML="<b>3</b>"
+
+document.body.appendChild(div)
+
+setTimeout(function(){
+let div=document.getElementById("countdown")
+div.innerHTML="<b>2</b>"
+
+document.body.appendChild(div)
+
+setTimeout(function(){
+let div=document.getElementById("countdown")
+div.innerHTML="<b>1</b>"
+
+document.body.appendChild(div)
+
+setTimeout(function(){
+let div=document.getElementById("countdown")
+div.innerHTML="<b>HAPPY INDIPENDENCE DAY</b>"
+
+document.body.appendChild(div)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
